80af7f4f6eddb97e2e106fa98feb8ce60ed104b0
Jen Merritt <jenjomez@gmail.com>
2020-03-25 14:22:56 +0000

    progressing predictions form - unique list of friends displaying in dropdown

--- frontend/src/components/AddPredictionForm.js
+++ frontend/src/components/AddPredictionForm.js
@@ -9,13 +9,15 @@ class AddPredictionForm extends Component {
           prediction: "",
           criteriaToUpdateId: null,
           predictionFormState: false,
-          predictionsForGame: []
+          predictionsForGame: [],
+          friendsListForGame: []
         };
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handlePredictionChange = this.handlePredictionChange.bind(this)
         this.updateCriteriaState = this.updateCriteriaState.bind(this)
         this.togglePredictionFormClass = this.togglePredictionFormClass.bind(this)
         this.fetchPredictions = this.fetchPredictions.bind(this)
+        this.setFriendsList = this.setFriendsList.bind(this)
     }
 
     fetchPredictions(){
@@ -24,11 +26,29 @@ class AddPredictionForm extends Component {
       this.props.selectedGame.criterias.map(criteria => {
         fetch(`http://localhost:8080/criterias/${criteria.id}/predictions`)
         .then(res => res.json())
-        .then(predictionsFetched => predictionsArray.push(predictionsFetched))
+        .then(predictionsFetched => 
+          predictionsArray.push(predictionsFetched),
+          this.setState({predictionsForGame: predictionsArray}))
+        .then(() => this.setFriendsList())
         .catch(err => console.error)
       })
-      
-      this.setState({predictionsForGame: predictionsArray})
+    }
+
+    setFriendsList(){
+      const predictionsArray = this.state.predictionsForGame
+      let friendsArray = []
+      let arrayToFilter = []
+      predictionsArray.map(prediction => {
+        let predictions = prediction._embedded.predictions
+        predictions.map(actualPrediction => {
+          let currentFriend = actualPrediction.friend
+          arrayToFilter.push(currentFriend)
+        })
+      })
+      friendsArray = arrayToFilter.filter((obj,pos,arr) => {
+        return arr.map(mapObj => mapObj.id).indexOf(obj.id) === pos;  
+      })
+      this.setState({friendsListForGame: friendsArray})
     }
 
     handlePredictionChange(event) {
@@ -65,14 +85,35 @@ class AddPredictionForm extends Component {
         this.setState({predictionFormState: !currentState})
       }
 
+      handleSelectFriend(event) {
+        console.log(event)
+        // this.setState({
+        //   selectedCriteria: null
+        // })
+        // const selectedGame = this.props.games.find(game => {
+        //   return game.id == event.target.value
+        // })
+        // this.setState({
+        //   selectedGame: selectedGame
+        // })
+      }
+
     render(){
 
         return(
 
             <>
-              <button className="add-prediction-button" onClick={this.togglePredictionFormClass, this.fetchPredictions}>Manage Predictions</button>
+              <button className="add-prediction-button" onClick={() => {this.togglePredictionFormClass(); this.fetchPredictions()}}>Manage Predictions</button>
               <section className={this.state.predictionFormState ? null : "hidden"}>
-                <p>Prediction Form goes here</p>
+                <p>blah</p>
+                  <article>
+                    <select onChange={this.handleSelectFriend} >
+                      <option disabled default selected>Select a Friend</option>
+                      {this.state.friendsListForGame.map(friend => {
+                        return <option value={friend.id} key={friend.id} >{friend.name}</option>
+                      })}
+                    </select>
+                </article>
               </section>
             </>
 
